182c32556be8f689022a66a6bb4d3d401f240f4e
Kevin <kayaswp@gmail.com>
2022-03-16 14:54:07 +0630

    create first route and file structure

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur necessitatibus non dolorem nihil, officiis veniam deserunt repellat debitis doloremque praesentium similique explicabo amet eius cum? Expedita aliquam natus explicabo ullam odit quia provident praesentium, maxime quas harum ipsam dolorum vel libero tempore, excepturi fuga ad laboriosam. Rem soluta qui, repellat optio hic reiciendis placeat voluptatum quam laborum corrupti aperiam harum a sequi ut consequuntur repudiandae similique? Exercitationem possimus provident obcaecati dolore inventore quis repudiandae magnam, sint maiores, necessitatibus esse ab adipisci eum quos! Ducimus ab at, consequatur velit eligendi, est voluptas saepe similique necessitatibus aperiam asperiores debitis? Libero, consectetur! Maiores?</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam architecto reiciendis aliquam, inventore possimus expedita tempora distinctio perspiciatis in obcaecati rem minus cumque enim deleniti exercitationem eaque vitae ratione error asperiores quos aspernatur animi. Quis, eligendi illo! Est tenetur magni neque voluptas! Ut minima perferendis expedita eligendi veniam quibusdam fugit, repudiandae illum? Doloremque, delectus? Culpa impedit facilis molestias expedita consequuntur accusamus quam et ab aliquam ipsam vel omnis error quae, corporis quia. Earum officia ullam quas incidunt sed minus corrupti doloremque impedit dicta harum est necessitatibus excepturi unde vel, quis laudantium, sunt quidem. Minus voluptates laboriosam, debitis impedit voluptatum aliquid?
+      </p>
+    
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
---
+++ src/views/jobs/JobsView.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>Jobs</h1>
+  <div class="color" v-for="job in jobs" :key="job.id">
+      <h1>{{job.position}} | {{job.hero}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            jobs:[
+                {id:"1",position:"Top laner",hero:"Jayce , Kennen , Rekneton"},
+                {id:"2",position:"Mid laner",hero:"Corki , Ahri , Brand"},
+                {id:"3",position:"ADC",hero:"Ashe , Ezreal , Varus"}
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+    .color{
+        color: crimson;
+    }
+</style>
